1fd5f47f033e8e2059d071051162bc00e918d1b8
xgk1806803zhg <1183515596@qq.com>
2025-05-09 00:48:52 +0800

    111

---
+++ MyAPP/x64/Debug/qt/moc/moc_AlgTreeWidget.cpp
@@ -0,0 +1,94 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'AlgTreeWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../AlgorithmicRegions/AlgTreeWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'AlgTreeWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_AlgTreeWidget_t {
+    QByteArrayData data[1];
+    char stringdata0[14];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_AlgTreeWidget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_AlgTreeWidget_t qt_meta_stringdata_AlgTreeWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 13) // "AlgTreeWidget"
+
+    },
+    "AlgTreeWidget"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_AlgTreeWidget[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void AlgTreeWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject AlgTreeWidget::staticMetaObject = { {
+    &QTreeWidget::staticMetaObject,
+    qt_meta_stringdata_AlgTreeWidget.data,
+    qt_meta_data_AlgTreeWidget,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *AlgTreeWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *AlgTreeWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_AlgTreeWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QTreeWidget::qt_metacast(_clname);
+}
+
+int AlgTreeWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QTreeWidget::qt_metacall(_c, _id, _a);
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ MyAPP/x64/Debug/qt/uic/ui_AlgorithmicRegion.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'AlgorithmicRegion.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.9
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ALGORITHMICREGION_H
+#define UI_ALGORITHMICREGION_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AlgorithmicRegionClass
+{
+public:
+
+    void setupUi(QWidget *AlgorithmicRegionClass)
+    {
+        if (AlgorithmicRegionClass->objectName().isEmpty())
+            AlgorithmicRegionClass->setObjectName(QString::fromUtf8("AlgorithmicRegionClass"));
+        AlgorithmicRegionClass->resize(600, 400);
+
+        retranslateUi(AlgorithmicRegionClass);
+
+        QMetaObject::connectSlotsByName(AlgorithmicRegionClass);
+    } // setupUi
+
+    void retranslateUi(QWidget *AlgorithmicRegionClass)
+    {
+        AlgorithmicRegionClass->setWindowTitle(QApplication::translate("AlgorithmicRegionClass", "AlgorithmicRegion", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AlgorithmicRegionClass: public Ui_AlgorithmicRegionClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ALGORITHMICREGION_H
